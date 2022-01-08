8cb3cfd3d7f809f8b1b44b6a1562948148b97abd
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2022-01-08 23:11:28 +0700

    add map

--- src/components/sections/OpeningPhrase.svelte
+++ src/components/sections/OpeningPhrase.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { Map, ArrowRight } from 'svelte-bootstrap-icons'
   import { data as langDataStore } from '../../stores/lang'
   import Carousel from './Carousel.svelte';
   import DateCard from './DateCard.svelte';
@@ -20,5 +21,17 @@
   >{$langDataStore?.section_date_place || 'section_date_place'}</p>
   <div class="py-2 px-4">
     <Carousel />
+  </div>
+  <div class="mt-5 d-flex justify-content-center">
+    <a
+    href="https://goo.gl/maps/Z4XUmgvTzHLhZVx37"
+    target="__blank"
+    class="btn btn-hirzi bold-text"
+    style="box-shadow: 0 0 48px 0 rgb(0 0 0 / 20%)"
+  >
+    <Map width="24" height="24" />
+    <span class="px-3">{$langDataStore?.open_map_button || 'open_map_button'}</span>
+    <ArrowRight width="24" height="24" />
+  </a>
   </div>
 </div>
