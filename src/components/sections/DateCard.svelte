3fc154304672c595cb5f2fa124ee23e298321962
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2022-01-25 16:21:00 +0700

    fix datecard dark mode

--- src/components/sections/DateCard.svelte
+++ src/components/sections/DateCard.svelte
@@ -4,28 +4,28 @@
 
 <div class="d-flex justify-content-center bold-text">
   <div style="margin: 25px 0" class="align-items-center">
-    <span class="primary-text primary-box box inner-box" style="padding: 1px 14px;">
+    <span class="primary-text-outside primary-box box inner-box" style="padding: 1px 14px;">
       {$langDataStore?.section_date_date[0] || 'section_date_date'}
     </span>
-    <span class="primary-text primary-box box inner-box">
+    <span class="primary-text-outside primary-box box inner-box">
       {$langDataStore?.section_date_date[1] || 'section_date_date'}
     </span>
     <span class="inner-box primary-text-outside" style="padding: 0 3px;">
       /
     </span>
-    <span class="primary-text primary-box box inner-box">
+    <span class="primary-text-outside primary-box box inner-box">
       {$langDataStore?.section_date_month[0] || 'section_date_month'}
     </span>
-    <span class="primary-text primary-box box inner-box">
+    <span class="primary-text-outside primary-box box inner-box">
       {$langDataStore?.section_date_month[1] || 'section_date_month'}
     </span>
     <span class="inner-box primary-text-outside" style="padding: 0 3px;">
       /
     </span>
-    <span class="primary-text primary-box box inner-box">
+    <span class="primary-text-outside primary-box box inner-box">
       {$langDataStore?.section_date_year[0] || 'section_date_year'}
     </span>
-    <span class="primary-text primary-box box inner-box">
+    <span class="primary-text-outside primary-box box inner-box">
       {$langDataStore?.section_date_year[1] || 'section_date_year'}
     </span>
   </div>
